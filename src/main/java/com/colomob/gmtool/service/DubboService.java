5a92c5781a3f0033dfcc4529788af06a02745bd4
baowp <122515909@qq.com>
2013-08-12 19:02:07 +0800

    add dubbo

---
+++ src/main/java/com/colomob/gmtool/service/DubboService.java
@@ -0,0 +1,19 @@
+/**
+ * Project: jpetstore-6
+ * 
+ * File Created at Aug 12, 2013
+ * $Id$Corporation
+ * 
+ * Copyright 2013-2015 Colomob.com Corporation Limited.
+ * All rights reserved.
+ */
+package com.colomob.gmtool.service;
+
+/**
+ * @author baowp
+ * 
+ */
+public interface DubboService {
+
+	String hello(String name);
+}
---
+++ src/main/java/com/colomob/gmtool/service/impl/DubboServiceImpl.java
@@ -0,0 +1,28 @@
+/**
+ * Project: jpetstore-6
+ * 
+ * File Created at Aug 12, 2013
+ * $Id$Corporation
+ * 
+ * Copyright 2013-2015 Colomob.com Corporation Limited.
+ * All rights reserved.
+ */
+package com.colomob.gmtool.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.colomob.gmtool.service.DubboService;
+
+/**
+ * @author baowp
+ * 
+ */
+@Service("dubboService")
+public class DubboServiceImpl implements DubboService {
+
+	public String hello(String name) {
+		System.out.println(name);
+		return name;
+	}
+
+}
